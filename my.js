aa02cac8a081b8af9f41c3aa5f7ce96332f8a580
marco <dobermannpoker86@gmail.com>
2020-04-01 13:49:05 +0200

    struttura grezza

---
+++ my.js
@@ -0,0 +1,5 @@
+console.log("ciao");
+                // parto da buttare in pagina gli elementi grezzi (non mi interessa la grafica per ora);
+                // ricreo la funzionalità dei multi dropdown sulle varie voci di menù, su viewport largo;
+                // ricreo anche la funzionalità di menù hamburger a viewport più piccoli;
+                // cerco di far si che funzioni la questione di far comparire e scomparire ciò che mi serve sui viewport diversi (quindi come abbiam fatto questa mattina sulla correzione EX che era di ieri).
