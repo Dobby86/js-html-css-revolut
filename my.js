98189685d6e4ad3f530402c2d8322f7dfeabb6b3
marco <dobermannpoker86@gmail.com>
2020-04-01 15:57:37 +0200

    toggle click aggiunto

--- my.js
+++ my.js
@@ -4,18 +4,17 @@ console.log("ciao");
                 // ricreo anche la funzionalità di menù hamburger a viewport più piccoli;
                 // cerco di far si che funzioni la questione di far comparire e scomparire ciò che mi serve sui viewport diversi (quindi come abbiam fatto questa mattina sulla correzione EX che era di ieri).
 
-// $(document).ready(function(){
-// 
-// $(".partedestra-content").hide();
-//
-// $("ul li").click(
-//     function(){
-//         $(this).show();
-//     }
-// )
-//
-//
-//
-//
-//
-// });
+$(document).ready(function(){
+
+
+
+$(".dropdown-content").hide();
+
+$(".dropdown a").click(
+    function(){
+        $(this).siblings(".dropdown-content").slideToggle();
+        // $(".dropdown-content").slideToggle();
+    }
+)
+
+});
