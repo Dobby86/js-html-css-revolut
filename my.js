639901075934ed508b082c7c1e784893c111427d
marco <dobermannpoker86@gmail.com>
2020-04-01 15:35:54 +0200

    aggiunta del dropdown

--- my.js
+++ my.js
@@ -3,3 +3,19 @@ console.log("ciao");
                 // ricreo la funzionalità dei multi dropdown sulle varie voci di menù, su viewport largo;
                 // ricreo anche la funzionalità di menù hamburger a viewport più piccoli;
                 // cerco di far si che funzioni la questione di far comparire e scomparire ciò che mi serve sui viewport diversi (quindi come abbiam fatto questa mattina sulla correzione EX che era di ieri).
+
+// $(document).ready(function(){
+// 
+// $(".partedestra-content").hide();
+//
+// $("ul li").click(
+//     function(){
+//         $(this).show();
+//     }
+// )
+//
+//
+//
+//
+//
+// });
